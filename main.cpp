ef4b1c9b129683361fe609954bb0c04d5ee820dc
sora <147121795+sora7716@users.noreply.github.com>
2025-05-13 17:46:41 +0900

    フラクタルアートを作成中

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
 int main() {
-
+	int depth = 10;
+	int width = 5;
+	//ê[Ç≥
+	for (int i = 0;i < depth;i++) {
+		//ïù
+		for (int j = 0;j < width + (i + 1);j++) {
+			if (j != width - (i + 1)) {
+				printf("-");
+			} else {
+				printf("*");
+			}
+		}
+		printf("\n");
+	}
 	return 0;
 }
