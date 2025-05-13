dc94d659ad8969819097c9f9bf9aded91042be3d
sora <147121795+sora7716@users.noreply.github.com>
2025-05-13 22:11:57 +0900

    フラクタルアートができた

--- main.cpp
+++ main.cpp
@@ -4,16 +4,12 @@ int main() {
 	//ê[Ç≥
 	const int kDepth = 5;
 	//ïù
-	const int kWidth = 5;
+	const int kWidth = 1;
 	//ê[Ç≥
 	for (int i = 0; i < kDepth; i++) {
 		//ïù
 		for (int j = 0; j < kWidth + i; j++) {
-			if (j < kWidth - (i + 1)) {
-				printf("-");
-			} else {
-				printf("*");
-			}
+			printf("*");
 		}
 		//â¸çs
 		printf("\n");
