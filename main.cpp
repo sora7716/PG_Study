7ca6190d0b33ad66fb9ff27e086bf95e0d69a0a2
sora <147121795+sora7716@users.noreply.github.com>
2025-05-15 18:57:06 +0900

    じゃんけんを作成中

--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
 #include <stdio.h>
 
+//じゃんけんの手
+enum Hand {
+	kGu,
+	kChoki,
+	kPa,
+	kHandCount,
+};
+
+/// <summary>
+/// じゃんけん
+/// </summary>
+/// <param name="player1">プレイヤー1</param>
+/// <param name="player2">プレイヤー2</param>
+void Battle(Hand player1, Hand player2) {
+
+}
+
 int main() {
 
 	return 0;
