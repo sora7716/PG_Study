ffb6b4aa048256fe1421dde6097d8804b202b8f7
sora <147121795+sora7716@users.noreply.github.com>
2025-06-04 12:12:04 +0900

    ループできるようにした

--- main.cpp
+++ main.cpp
@@ -3,6 +3,19 @@
 #include <time.h>
 #include <ctype.h>
 #include <string>
+/// <summary>
+/// 改行を削除
+/// </summary>
+/// <param name="input">入力</param>
+void DeleteReturn(char* input) {
+	//文字入力の冗長
+	char inputBuffer = '\0';
+	//改行を排除
+	while ((inputBuffer = getchar()) != '\n') {
+		input = &inputBuffer;
+	}
+
+}
 
 int main() {
 	//乱数生成器の初期化
@@ -17,6 +30,8 @@ int main() {
 		printf("丁を選ぶ場合は0を入力\n半を選ぶ場合は1を入力\n");
 		//プレイヤーの答え(受け取る)
 		playerAnswer = getchar();
+		//改行を削除
+		DeleteReturn(&playerAnswer);
 
 		//数字かどうかと数字が0または1かどうかのチェック
 		if (!isdigit(playerAnswer)) {
@@ -24,37 +39,49 @@ int main() {
 			continue;
 		} else if (atoi(&playerAnswer) > 2) {
 			printf("0または1を入力してください\n");
+			continue;
 		}
-	}
 
-	//プレイヤーの答えを整数に変換
-	int playerAnswerInt = atoi(&playerAnswer);
+		//プレイヤーの答えを整数に変換
+		int playerAnswerInt = atoi(&playerAnswer);
 
-	for (int i = 0; i < 2; i++) {
-		//さいころを振る
-		diceRoll[i] = rand() % 6 + 1;
-		printf("賽の目 : %d\n", diceRoll[i]);
-	}
+		//さいころの数分ループさせる
+		for (int i = 0; i < 2; i++) {
+			//さいころを振る
+			diceRoll[i] = rand() % 6 + 1;
+			printf("賽の目 : %d\n", diceRoll[i]);
+		}
 
-	//さいころの合計を計算
-	int diceRollSum = diceRoll[0] + diceRoll[1];
-	printf("賽の目の合計 : %d\n", diceRollSum);
+		//さいころの合計を計算
+		int diceRollSum = diceRoll[0] + diceRoll[1];
+		printf("賽の目の合計 : %d\n", diceRollSum);
 
-	//勝ち負けを判別
-	if (diceRollSum % 2 == 0) {
-		if (playerAnswerInt == 0) {
-			result = "勝ち";
+		//勝ち負けを判別
+		if (diceRollSum % 2 == 0) {
+			if (playerAnswerInt == 0) {
+				result = "勝ち";
+			} else {
+				result = "負け";
+			}
 		} else {
-			result = "負け";
+			if (playerAnswerInt == 0) {
+				result = "負け";
+			} else {
+				result = "勝ち";
+			}
 		}
-	} else {
-		if (playerAnswerInt == 0) {
-			result = "負け";
+
+		printf("結果 : %s", result.c_str());
+
+		//再度プレイするかどうかの確認
+		int isFinish = -1;
+		printf("\nもう一度プレイしますか？(0:はい, 1:いいえ)\n");
+		scanf_s("%d", &isFinish);
+		if (isFinish == 0) {
+			break;
 		} else {
-			result = "勝ち";
+			continue;
 		}
 	}
-
-	printf("結果 : %s", result.c_str());
 	return 0;
 }
