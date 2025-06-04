c60def944b2ce45fb03283b3f012cee48ef5fdc8
sora <147121795+sora7716@users.noreply.github.com>
2025-06-04 12:06:05 +0900

    いったん丁半博打は作成できた 現在はループするようにしている

--- main.cpp
+++ main.cpp
@@ -1,8 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <ctype.h>
+#include <string>
 
 int main() {
-	 
+	//乱数生成器の初期化
+	srand(static_cast<unsigned int>(time(nullptr)));
+	//さいころ2つ分
+	int diceRoll[2] = {};
+	//最終結果を表示
+	std::string result = "\0";
+	//プレイヤーの答え
+	char playerAnswer = '\0';
+	while (true) {
+		printf("丁を選ぶ場合は0を入力\n半を選ぶ場合は1を入力\n");
+		//プレイヤーの答え(受け取る)
+		playerAnswer = getchar();
+
+		//数字かどうかと数字が0または1かどうかのチェック
+		if (!isdigit(playerAnswer)) {
+			printf("数字を入力してください\n");
+			continue;
+		} else if (atoi(&playerAnswer) > 2) {
+			printf("0または1を入力してください\n");
+		}
+	}
+
+	//プレイヤーの答えを整数に変換
+	int playerAnswerInt = atoi(&playerAnswer);
+
+	for (int i = 0; i < 2; i++) {
+		//さいころを振る
+		diceRoll[i] = rand() % 6 + 1;
+		printf("賽の目 : %d\n", diceRoll[i]);
+	}
+
+	//さいころの合計を計算
+	int diceRollSum = diceRoll[0] + diceRoll[1];
+	printf("賽の目の合計 : %d\n", diceRollSum);
+
+	//勝ち負けを判別
+	if (diceRollSum % 2 == 0) {
+		if (playerAnswerInt == 0) {
+			result = "勝ち";
+		} else {
+			result = "負け";
+		}
+	} else {
+		if (playerAnswerInt == 0) {
+			result = "負け";
+		} else {
+			result = "勝ち";
+		}
+	}
+
+	printf("結果 : %s", result.c_str());
 	return 0;
 }
