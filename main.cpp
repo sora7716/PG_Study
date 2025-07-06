9c6a9228ae614384e1c11c634b90486aa28727b7
sora <147121795+sora7716@users.noreply.github.com>
2025-07-06 17:59:13 +0900

    シェルソートができた

--- main.cpp
+++ main.cpp
@@ -18,6 +18,8 @@ int main() {
 	srand(static_cast<unsigned int>(time(nullptr)));
 	//ソートするリスト
 	int lists[kListSize] = {};
+	//検索範囲
+	int range = kListSize - 1;
 	//リストの初期化
 	for (int i = 0; i < kListSize; i++) {
 		lists[i] = rand() % 100; // 0〜99の乱数を生成
@@ -27,14 +29,26 @@ int main() {
 	ListPrint(lists);
 
 	//挿入ソート開始
-	for (int i = 0; i < kListSize; i++) {
-		int temp = lists[i]; //挿入する値を一時保存
-		int index = i-1;
-		while (index >= 0 && lists[index] > temp) {
-			lists[index + 1] = lists[index];//右に移動
-			index--;
+	while (range > 0) {
+		//ここは挿入ソート
+		for (int i = 0; i < kListSize; i++) {
+			int temp = lists[i]; //挿入する値を一時保存
+			int index = i - 1;
+			while (index >= 0 && lists[index] > temp) {
+				lists[index + 1] = lists[index];//右に移動
+				index--;
+			}
+			lists[index + 1] = temp;
 		}
-		lists[index + 1] = temp;
+
+		//ここがシェルソートで追加された部分
+		if (range / 2 != 0) {
+			range /= 2; //範囲を半分にする
+		} else if (range == 1) {
+			range = 0;//範囲が1のときは終了
+		} else {
+			range = 1;//範囲が偶数のときは1にする
+		}
 	}
 
 	//ソート後のリストを出力
