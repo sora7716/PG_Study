43a45c0e29a486d0488bd8e01919ffd2ca11753a
sora <147121795+sora7716@users.noreply.github.com>
2025-07-06 17:35:39 +0900

    バブルソートが完成した

--- main.cpp
+++ main.cpp
@@ -1,6 +1,44 @@
-#include <stdio.h>
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+//リストの要素数
+const int kListSize = 10;
+
+//リストの出力
+void ListPrint(int* lists) {
+	for (int i = 0; i < kListSize; i++) {
+		printf("%d,", lists[i]);
+	}
+	printf("\n");
+}
 
 int main() {
+	//ランド関数の初期化
+	srand(static_cast<unsigned int>(time(nullptr)));
+	//ソートするリスト
+	int lists[kListSize] = {};
+	//リストの初期化
+	for (int i = 0; i < kListSize; i++) {
+		lists[i] = rand() % 100; // 0〜99の乱数を生成
+	}
+	//ソート前のリストを出力
+	printf("ソート前のリスト:");
+	ListPrint(lists);
 
+	//バブルソート開始
+	for (int i = 0; i < kListSize; i++) {
+		for (int j = kListSize - 1; j > i; j--) {
+			if (lists[j - 1] > lists[j]) {
+				int temp = lists[j - 1];
+				lists[j - 1] = lists[j];
+				lists[j] = temp;
+			}
+		}
+	}
+
+	//ソート後のリストを出力
+	printf("ソート後のリスト:");
+	ListPrint(lists);
 	return 0;
 }
