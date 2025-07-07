e9a5eb74639199a7f8dec4e5d41290cd2164ab1c
sora <147121795+sora7716@users.noreply.github.com>
2025-07-07 11:22:51 +0900

    シェルソートを実装し、関数化した

--- main.cpp
+++ main.cpp
@@ -13,6 +13,39 @@ void ListPrint(int* lists) {
 	printf("\n");
 }
 
+/// <summary>
+/// シェルソート
+/// </summary>
+/// <param name="lists">リスト</param>
+void ShellSort(int* lists) {
+	//検索範囲
+	int range = kListSize;
+
+	while (range > 0) {
+		//検索範囲分の挿入ソートを実行
+		for (int i = range; i < kListSize; i++) {
+			int temp = lists[i];
+			int index = i - 1;
+			while (index >= range && lists[index - range] > temp) {
+				lists[index] = lists[index - range];
+				index -= range;
+			}
+			lists[index] = temp;
+		}
+
+		//検索範囲を半分にする
+		if (range / 2 != 0) {
+			range /= 2;
+		} else if (range == 1) {
+			range = 0;// ループを終了するために0に設定
+		} else {
+			range = 1;// 1に設定して次のループで終了
+		}
+	}
+}
+
+
+
 int main() {
 	//ランド関数の初期化
 	srand(static_cast<unsigned int>(time(nullptr)));
@@ -28,28 +61,8 @@ int main() {
 	printf("ソート前のリスト:");
 	ListPrint(lists);
 
-	//挿入ソート開始
-	while (range > 0) {
-		//ここは挿入ソート
-		for (int i = 0; i < kListSize; i++) {
-			int temp = lists[i]; //挿入する値を一時保存
-			int index = i - 1;
-			while (index >= 0 && lists[index] > temp) {
-				lists[index + 1] = lists[index];//右に移動
-				index--;
-			}
-			lists[index + 1] = temp;
-		}
-
-		//ここがシェルソートで追加された部分
-		if (range / 2 != 0) {
-			range /= 2; //範囲を半分にする
-		} else if (range == 1) {
-			range = 0;//範囲が1のときは終了
-		} else {
-			range = 1;//範囲が偶数のときは1にする
-		}
-	}
+	//シェルソートを実行
+	ShellSort(lists);
 
 	//ソート後のリストを出力
 	printf("ソート後のリスト:");
